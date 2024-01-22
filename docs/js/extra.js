b820d91f99b4614d1996ddd748f9f401b76fd25f
abdulkadir <>
2024-01-22 08:14:45 +0300

    test js

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -18,3 +18,22 @@ function loadNavpane() {
         }
     }
 }
+
+async function fetchData() {
+    console.log("fetching data");
+    const url = 'https://crm.eblasoft.com.tr/api/v1/Docs?id=636514b37aee0f43f';
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const {version} = data;
+    const badgeUrl = `https://img.shields.io/badge/version-${version}-green`;
+
+    const badgeImg = document.createElement("img");
+    badgeImg.src = badgeUrl;
+    badgeImg.style = "height: 22px; margin-left: 10px;";
+
+
+    document.getElementById("ext-version").appendChild(badgeImg);
+}
+
+fetchData();
